cf3b03321c39e74e9aeaeaa2d3bcd8de6fc7ee42
hiro <h.n19960225@icloud.com>
2021-02-16 22:09:36 +0900

    Updates

---
+++ precache-manifest.08ff4e387ba37c5ca32d95a8e69b7868.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94db87fd1d104de1c6fca1cfaae9056e",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "8f8d78938cb37b1753ec",
+    "url": "/myapp/static/css/main.b257b1ee.chunk.css"
+  },
+  {
+    "revision": "cb75e2e6d2a28928b5e4",
+    "url": "/myapp/static/js/2.3ead4107.chunk.js"
+  },
+  {
+    "revision": "afda2700f6da2ba0c18fd4360dcac5ae",
+    "url": "/myapp/static/js/2.3ead4107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f8d78938cb37b1753ec",
+    "url": "/myapp/static/js/main.9a9ecdf8.chunk.js"
+  },
+  {
+    "revision": "217a51fda8d5ee214eb8",
+    "url": "/myapp/static/js/runtime-main.9011cbcc.js"
+  },
+  {
+    "revision": "3786d5b87a9c638340266d979918f481",
+    "url": "/myapp/static/media/computer-icon.3786d5b8.png"
+  },
+  {
+    "revision": "a69029e0dc45754cdc9d53af0739fcec",
+    "url": "/myapp/static/media/user.a69029e0.png"
+  }
+]);
